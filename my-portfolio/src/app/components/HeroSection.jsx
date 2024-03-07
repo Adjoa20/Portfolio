478b0b4d58f08176d28aad702c2fd0f4f35fdc2a
agathaa <aamoako1120@gmail.com>
2024-03-07 13:37:59 -0500

    added animation for header

--- my-portfolio/src/app/components/HeroSection.jsx
+++ my-portfolio/src/app/components/HeroSection.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -7,8 +10,28 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm-text-left">
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-orange-200 to-yellow-100'> Welcome, {" "} </span>
-            My Name Is Agatha{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-200 to-yellow-100">
+              {" "}
+              Welcome,{" "}
+            </span>
+            My Name Is Agatha And I Am A {" "}
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Web Developer",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Backend Developer",
+                1000,
+                "Backend DevOps",
+                1000,
+                "Full Stack Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "2em", display: "inline-block" }}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl">
             I am a Full Stack Developer who can build you furniture or a website
@@ -19,19 +42,25 @@ const HeroSection = () => {
             function AND I can make it look good!
           </p>
           <div>
-          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black'> Hire Me </button>
-          <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'> Download Resume </button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              {" "}
+              Hire Me{" "}
+            </button>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+              {" "}
+              Download Resume{" "}
+            </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0 flex justify-center">
-        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative justify-center">
-          <Image
-            src="/images/profilepic.png"
-            alt="profilepic"
-            className="rounded-full"
-            width={400}
-            height={400}
-          />
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative justify-center">
+            <Image
+              src="/images/profilepic.png"
+              alt="profilepic"
+              className="rounded-full"
+              width={400}
+              height={400}
+            />
           </div>
         </div>
       </div>
