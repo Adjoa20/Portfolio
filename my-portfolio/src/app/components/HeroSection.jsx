a8cb5e1728ef2076d366a4ef99584f5ee600e020
agathaa <aamoako1120@gmail.com>
2024-03-07 13:22:13 -0500

    updated header

--- my-portfolio/src/app/components/HeroSection.jsx
+++ my-portfolio/src/app/components/HeroSection.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            {" "}
-            Welcome, My Name Is Agatha{" "}
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm-text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-orange-200 to-yellow-100'> Welcome, {" "} </span>
+            My Name Is Agatha{" "}
           </h1>
-          <p className="text-[#adb7be] text-lg mb-6 lg:text-xl">
+          <p className="text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl">
             I am a Full Stack Developer who can build you furniture or a website
             and project/product manage with efficiency and success. I have a
             knack for fixing and building anything and have a passion for
@@ -19,8 +19,8 @@ const HeroSection = () => {
             function AND I can make it look good!
           </p>
           <div>
-          <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'> Hire Me </button>
-          <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'> Download Resume </button>
+          <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black'> Hire Me </button>
+          <button className='px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'> Download Resume </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0 flex justify-center">
