57702976d20c15619c763a09c7ee3a5daf865845
agathaa <aamoako1120@gmail.com>
2024-03-08 13:57:45 -0500

    added icons

--- my-portfolio/src/app/components/NavBar.jsx
+++ my-portfolio/src/app/components/NavBar.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import NavLink from "./NavLink";
+import { Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
+
 
 const navLinks = [
   {
@@ -18,21 +21,36 @@ const navLinks = [
 ];
 
 const NavBar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggleNavBar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
   return (
-    <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto p-8">
-        <Link href={"/"} className="text-5xl text-white font-semibold">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
+        <Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
           HOME
         </Link>
-        <div className="menu block md:w-auto" id="navbar"></div>
+        <div className="mobile-menu block md:hidden">
+          <button
+            onClick={toggleNavBar}
+            className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white "
+          >
+           { navbarOpen ? < XMarkIcon className="h-5 w-5"/> : <Bars3Icon className="h-5 w-5" />} 
+          </button>
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar"></div>
         <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-            {
-                navLinks.map((link, index) => (
-                    <li key={index}>
-                        <NavLink href={link.path} title={link.title}/>
-                    </li>
-                ))
-            }
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavLink href={link.path} title={link.title} />
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
