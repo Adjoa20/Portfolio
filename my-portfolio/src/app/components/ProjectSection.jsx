54b0ddac3bf4470e5d4618c1855f62841b5607b6
agathaa <aamoako1120@gmail.com>
2024-03-08 20:37:00 -0500

    updated project section

--- my-portfolio/src/app/components/ProjectSection.jsx
+++ my-portfolio/src/app/components/ProjectSection.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
-import Image from "next/image"
+import ProjectTags from "./ProjectTags";
+
+
 
 const projectsData = [
   {
@@ -34,12 +38,20 @@ const projectsData = [
 ];
 
 const ProjectSection = () => {
+    const [tag, setTag ] = useState("All")
+
+    const handleTagChange = (newTag) => {
+        setTag(newTag)
+    }
+    const fliteredProjects = projectsData.filter((project) => {
+        project.tag.includes(tag)
+    })
   return (
     <>
       <h2 className="text-center text-4xl font-bold text-white mt-4">My Projects</h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6 ">
-        <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">All</button>
-        <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">Web</button>
+        <ProjectTags onClick={handleTagChange} name="All" isSelected={tag === "All"}/>
+        <ProjectTags onClick={handleTagChange} name="Web" isSelected={tag === "Web"}/>
       </div>
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
         {projectsData.map((project) => (
