20a8002530b536ceb3e195451d7a81d0cfad2acb
agathaa <aamoako1120@gmail.com>
2024-03-10 00:47:30 -0500

    updating links

--- my-portfolio/src/app/components/NavLink.jsx
+++ my-portfolio/src/app/components/NavLink.jsx
@@ -1,11 +1,28 @@
+'use client'
+
+// import Link from "next/link";
+
+// const NavLink = ({ href, title }) => {
+//   return (
+//     <Link
+//       href={href}>
+//       <a className="block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded md:p-0 hover:text-white"> {title}</a>
+//    </Link>
+//   );
+// };
+
+// export default NavLink;
+
+
+// NavLink.js
+import React from "react";
 import Link from "next/link";
 
 const NavLink = ({ href, title }) => {
   return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#adb7be] sm:text-xl rounded md:p-0 hover:text-white"
-    >{title}</Link>
+    <Link href={href}>
+      <a className="text-white hover:text-slate-200">{title}</a>
+    </Link>
   );
 };
 
